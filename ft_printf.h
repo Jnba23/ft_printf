78ef0608441bf0d358a817df50c68971cc851469
Abdelwadoud Sayad <asayad@e1r8p5.1337.ma>
2024-05-31 16:56:49 +0100

    ft_printf

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: asayad <asayad@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 15:38:01 by asayad            #+#    #+#             */
-/*   Updated: 2024/05/12 10:13:21 by asayad           ###   ########.fr       */
+/*   Updated: 2024/05/31 16:09:52 by asayad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,5 @@ int		ft_printf(const char *str, ...);
 void	ft_putchar(char c, int *p);
 void	ft_putstr(char *s, int *p);
 void	ft_ptnbb(long long x, char *base, char c, int *p);
-size_t 	ft_strlen(const char *s);
+size_t	ft_sstrlen(const char *s);
 #endif
--- main.c
+++
@@ -1,9 +0,0 @@
-#include "ft_printf.h"
-#include "libc.h"
-// #include <fcntl.h>
-// #include "errno.h"
-
-int main()
-{
-    ft_printf("%%"); 
-}   
